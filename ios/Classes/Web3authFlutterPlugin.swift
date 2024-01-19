cffd63d9c50409f8c9ab3846ddf519ba4a9d27a2
Kenson G <kenspy99@gmail.com>
2024-01-19 18:25:31 -0500

    Update Web3authFlutterPlugin.swift

--- ios/Classes/Web3authFlutterPlugin.swift
+++ ios/Classes/Web3authFlutterPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Web3Auth
 
 public class Web3authFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -7,6 +8,13 @@ public class Web3authFlutterPlugin: NSObject, FlutterPlugin {
     let instance = Web3authFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
+
+  var web3auth: Web3Auth?
+  public var state: Web3AuthState? {
+      return web3auth?.state
+  }
+  var decoder = JSONDecoder()
+  var encoder = JSONEncoder()
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
