138294d97c563d9cac303855162c291a0d215296
Kenson G <kenspy99@gmail.com>
2024-01-19 21:43:39 -0500

    Removed macOS files

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import web3auth_flutter
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  Web3AuthFlutterPlugin.register(with: registry.registrar(forPlugin: "Web3AuthFlutterPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <web3auth_flutter/web3_auth_flutter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  Web3AuthFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Web3AuthFlutterPlugin"));
+}
